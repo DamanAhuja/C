d97da556be2f9c80ecb5deaf9bf7df60344d9f02
Daman Ahuja <142963733+DamanAhuja@users.noreply.github.com>
2024-02-20 00:39:08 +0530

    Create addnodeafternode.c

---
+++ linked llist/addnodeafternode.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+void view(struct node *head) {
+    while (head != NULL) {
+        printf("%d\n",head -> data);
+        head = head -> next;
+    }
+}
+
+void add(struct node *find,struct node *head, struct node *add) {
+    while (head != NULL && head != find) {
+        head = head -> next;
+    }
+    add -> next = head -> next;
+    head -> next = add;
+}
+
+int main() {
+    
+    struct node head;
+    struct node *a;
+    struct node *b;
+    struct node *c;
+    struct node *d;
+    struct node *e;
+
+    a=(struct node* )malloc(sizeof(struct node));
+    b=(struct node* )malloc(sizeof(struct node));
+    c=(struct node* )malloc(sizeof(struct node));
+    d=(struct node* )malloc(sizeof(struct node));
+    e=(struct node* )malloc(sizeof(struct node));
+
+    head.data = 0;
+    head.next = a;
+
+    a -> data = 1;
+    a -> next = b;
+
+    b -> data = 2;
+    b -> next = c;
+
+    c -> data = 3;
+    c -> next = d;
+
+    d -> data = 4;
+    d -> next = NULL;
+    
+    e -> data = 5;
+    e -> next = NULL;
+    
+    printf("Before adding: \n");
+    view(head.next);
+    add(d,head.next,e);
+    printf("After adding node: \n");
+    view(head.next);
+
+    free(a);
+    free(b);
+    free(c);
+    free(d);
+    free(e);
+
+    a = NULL;
+    b = NULL;
+    c = NULL;
+    d = NULL;
+    e = NULL;
+
+    return 0;
+}
