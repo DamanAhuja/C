ec87ebaa3e4cd6aecc5828d9b478fcf73a574ffc
Daman Ahuja <142963733+DamanAhuja@users.noreply.github.com>
2024-02-21 18:24:42 +0530

    Update create.c

--- double-linked-list/create.c
+++ double-linked-list/create.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct node {
+    struct node *lp;
+    int data;
+    struct node *rp;
+} sn;
+
+int main() {
+    
+    sn *one;
+    sn *two;
+    sn *three;
+    sn *four;
+    
+    one = (sn *)malloc(sizeof(sn));
+    two = (sn *)malloc(sizeof(sn));
+    three = (sn *)malloc(sizeof(sn));
+    four = (sn *)malloc(sizeof(sn));
+    
+    one->lp = NULL;
+    one->data = 1;
+    one->rp = two;
+    
+    two->lp = one;
+    two->data = 1;
+    two->rp = three;
+    
+    three->lp = two;
+    three->data = 3;
+    three->rp = four;
+    
+    four->lp = three;
+    four->data = 4;
+    four->rp = NULL;
+    
+    free(one);
+    one = NULL;
+    
+    free(two);
+    two = NULL;
+    
+    free(three);
+    three = NULL;
+    
+    free(four);
+    four = NULL;
+    
+    printf("Done");
+    
+    return;
+}
